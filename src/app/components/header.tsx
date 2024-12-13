f2c383da80ecd38ccc53b42eb73ae0c4879ca6a1
Sumit9328 <sumitdhir171@gmail.com>
2024-12-13 15:01:06 +0530

    13dec24

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -154,7 +154,7 @@ const Header = () => {
                     <svg  xmlns="http://www.w3.org/2000/svg"  width="14"  height="14"  viewBox="0 0 24 24"  fill="currentColor"  className="icon icon-tabler icons-tabler-filled icon-tabler-caret-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11.293 7.293a1 1 0 0 1 1.32 -.083l.094 .083l6 6l.083 .094l.054 .077l.054 .096l.017 .036l.027 .067l.032 .108l.01 .053l.01 .06l.004 .057l.002 .059l-.002 .059l-.005 .058l-.009 .06l-.01 .052l-.032 .108l-.027 .067l-.07 .132l-.065 .09l-.073 .081l-.094 .083l-.077 .054l-.096 .054l-.036 .017l-.067 .027l-.108 .032l-.053 .01l-.06 .01l-.057 .004l-.059 .002h-12c-.852 0 -1.297 -.986 -.783 -1.623l.076 -.084l6 -6z" /></svg>
                     </span>
                   </button>
-                  
+
                   {openMenu === "services" && (
                     <div className="absolute left-0 mt-2 w-[280px] bg-white shadow-md rounded-md transition-all ease-in-out">
                       <ul className="pl-5 py-2 space-y-2 mt-4">
@@ -237,20 +237,36 @@ const Header = () => {
                   <span className="ml-2 font-[400]">{selectedCountry}</span>
                 </div>
                 {openMenu === "countryDropdown" && (
-                  <ul className="absolute font-[400] pr-4 bg-white shadow-md rounded-md absolute top-[25px] right-[-20px] mt-1 py-1">
-                    {["UAE", "USA", "China", "India", "Germany"].map((country) => (
-                      <li
-                        key={country}
-                        className="px-1 py-2 hover:text-[#0073fd] cursor-pointer "
-                        onClick={() => handleCountrySelect(country)}
-                      >
-                        {country}
-                      </li>
-                    ))}
-                  </ul>
+                  <ul className="absolute font-[400] pr-4 bg-white shadow-md rounded-md top-[20px] right-[-5px] mt-1 py-1">
+                  <li className="px-1 py-2 hover:text-[#0073fd] cursor-pointer">
+                    <Link href="/allUAEcomponents">
+                      <span className="text-inherit">UAE</span>
+                    </Link>
+                  </li>
+                  <li className="px-1 py-2 hover:text-[#0073fd] cursor-pointer">
+                    <Link href="/usa">
+                      <span className="text-inherit">USA</span>
+                    </Link>
+                  </li>
+                  <li className="px-1 py-2 hover:text-[#0073fd] cursor-pointer">
+                    <Link href="/china">
+                      <span className="text-inherit">China</span>
+                    </Link>
+                  </li>
+                  <li className="px-1 py-2 hover:text-[#0073fd] cursor-pointer">
+                    <Link href="/india">
+                      <span className="text-inherit">India</span>
+                    </Link>
+                  </li>
+                  <li className="px-1 py-2 hover:text-[#0073fd] cursor-pointer">
+                    <Link href="/germany">
+                      <span className="text-inherit">Germany</span>
+                    </Link>
+                  </li>
+                </ul>
                 )}
               </div>
-              <button className="px-4 py-2 bg-gray-800 font-[400] text-white w-[150px] rounded-[50px] hover:bg-gray-700">
+              <button className="px-4 py-1 bg-gray-800 font-[400] text-white w-[150px] rounded-[50px] hover:bg-gray-700">
                 Log In
               </button>
             </div>
